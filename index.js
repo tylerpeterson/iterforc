c2560f84a45a39d38aa07cbae59af6b994dce243
Tyler Peterson <petersontb@familysearch.org>
2014-11-11 09:43:51 -0700

    increase project model

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-var teams = require('./teams');
+var project = require('./project');
+var teams = project.teams;
+var startDate = new Date(project.schedule.startDate);
 
+console.log(startDate);
 console.log(teams[0].name);
+
+
